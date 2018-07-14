cca1438b8192f35c4c296ca7840df8be5315b6ff
王含泽 <whzcorcd@gmail.com>
2018-07-14 17:39:34 +0800

    本地化修改

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ddns import *
 
+
 def main():
-	ddns=DDNS("LOGIN_ID","LOGIN_TOKEN")
-	ddns.update()
+    ddns = DDNS("61083", "30da406fb516e52be45ed7ec92b58323")
+    ddns.update()
+
 
 if __name__ == '__main__':
-	main()
+    main()
